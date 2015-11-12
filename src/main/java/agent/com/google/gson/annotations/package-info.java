1b5df15fb0e3331aa8c878ae10df61f8ea7ccfc9
sky <sky@sky-pc>
2015-11-12 15:40:50 +0800

    提交Google gson包，为项目提供json支持

---
+++ src/main/java/agent/com/google/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link agent.com.google.gson.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package agent.com.google.gson.annotations;
